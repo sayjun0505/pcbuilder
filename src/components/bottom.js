678d6c8b91655c9f081b65c99b88b455710f2362
sayjun0505 <sayjun0505@gmail.com>
2024-06-26 05:43:09 +0530

    a

--- src/components/bottom.js
+++ src/components/bottom.js
@@ -1,5 +1,35 @@
 export default function Bottom() {
     return (
-        <div>I am Bottom.</div>
+        <div className="w-full my-12 px-32">
+            <div className="w-full border-t-2 border-b-2 border-gray-300 flex flex-row justify-between py-12">
+                <div className="flex w-2/5 flex-col gap-4">
+                    <img id="logo-image" src="https://pc-builder.io/img/pc-builder-logo.svg" alt="PC Builder" className="w-48" />
+                    <span>Browse, Discover, Customize, Build Your PC - All in one place!</span>
+                    <span>
+                        PC Builder is your go-to platform for building your PC from scratch. You get a variety of computer parts at your disposal from all over the internet, including every major e-commerce website.
+                    </span>
+                    <span>Reach us at : contact@pc-builder.io</span>
+                </div>
+                <div className="w-1/4 flex flex-col gap-4">
+                    <span className="font-bold">Disclaimer</span>
+                    <span>Product price & specifications may change from time-to-time. So, before making a purchase, double-check them on the retailer's website.</span>
+                    <span>Although, the compatibility filter is highly accurate, just verify before ordering them.</span>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <span className="font-bold">Useful Links</span>
+                    <span>Power Supply Calculator</span>
+                    <span>Product List</span>
+                    <span>Blog</span>
+                    <span>About Us</span>
+                    <span>Contact Us</span>
+                    <span>Terms & Conditions</span>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <span className="font-bold">Connect</span>
+                    <span>Facebook</span>
+                    <span>Twitter</span>
+                </div>
+            </div>
+        </div>
     )
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,14 @@
 export default function Footer() {
     return (
-        <div>I am Footer.</div>
+        <div className="px-32">
+            <div className="flex flex-row items-center rounded-xl p-12 text-white justify-between w-full bg-[#5c4ee2]">
+                <div className=""></div>
+                <div className="flex flex-col gap-4">
+                    <div className="text-left font-bold text-2xl">Have A Query?</div>
+                    <div className="text-left text-xl">Get In Touch With Our Experts To Get It Resolved.</div>
+                </div>
+                <div className="px-12 py-4 rounded-lg bg-white w-fit text-gray-800 items-center">Contact Us</div>
+            </div>
+        </div>
     )
 }
