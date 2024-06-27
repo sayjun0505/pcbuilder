72815ee8fcc4ad98971cf170650c8b9bb6ef7a23
sayjun0505 <sayjun0505@gmail.com>
2024-06-26 21:59:08 -0700

    a

--- src/pages/info.js
+++ src/pages/info.js
@@ -8,8 +8,165 @@ export default function Info() {
     // Example: Display the data
     return (
         <div>
-            <h2>Info Component</h2>
-            <pre>{JSON.stringify(data, null, 2)}</pre>
+            
+            <div className='w-full rounded-xl p-8 flex flex-col gap-8'>
+                <div className='flex flex-row justify-center items-center w-full gap-12'>
+                    <img src="https://pc-builder.io/images/cpu/1692871336_61345.jpg" alt="AMD Ryzen 7 7800X3D 4.2 GHz 8-Core Processor" className='w-48'/>
+                    <div className='flex flex-col gap-4'>
+                        <span className='mt-4 text-4xl font-bold'>AMD Ryzen 7 7800X3D 4.2 GHz 8-Core Processor</span>
+                        <div className='text-right'>registerID:{JSON.stringify(data, null, 2)}</div>
+                    </div>
+
+                </div>
+                <div className='mt-8 w-full flex flex-row justify-between'>
+                    <div className='w-[37%] flex flex-col'>
+                        <div className='w-full rounded-lg text-center bg-[#ecf1fe] p-4'>Specifications</div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Manufacturer</span>
+                            <span className=''>AMD</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Part #</span>
+                            <span className=''>100-100000910WOF</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Core Count</span>
+                            <span className=''>8</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Core Clock</span>
+                            <span className=''>4.2 GHz</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Boost Clock</span>
+                            <span className=''>5 GHz</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>TDP</span>
+                            <span className=''>120 W</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Series</span>
+                            <span className=''>AMD Ryzen 7</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Microarchitecture</span>
+                            <span className=''>Zen 4</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Core Family</span>
+                            <span className=''>Raphael</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Socket</span>
+                            <span className=''>AM5</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Integrated Graphics</span>
+                            <span className=''>Radeon</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Maximum Supported Memory</span>
+                            <span className=''>128 GB</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>ECC Support</span>
+                            <span className=''>Yes</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Includes Cooler</span>
+                            <span className=''>No</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Packaging</span>
+                            <span className=''>Boxed</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>L1 Cache</span>
+                            <div className='flex flex-col items-end'>
+                                <span className=''>8 x 32 kB Instruction</span>
+                                <span className=''>8 x 32 kB Data</span>
+                            </div>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>L2 Cache</span>
+                            <span className=''>8 x 1 MB</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>L3 Cache</span>
+                            <span className=''>1 x 96 MB</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Lithography</span>
+                            <span className=''>5 nm</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Includes CPU Cooler</span>
+                            <span className=''>No</span>
+                        </div>
+                        <div className='flex flex-row justify-between h-16 items-center px-4 border-b-2 border-gray-300'>
+                            <span className=''>Simultaneous Multithreading</span>
+                            <span className=''>Yes</span>
+                        </div>
+                    </div>
+                    <div className='w-[60%]'>
+                        <div className='bg-[#c2cad0] w-full h-32 text-center flex items-center justify-center text-6xl text-bold'>Price Tracker</div>
+                        <div className='relative w-full h-fit bg-[#dfdfdf]'>
+                            <div className='py-20  w-full flex flex-col gap-2'>
+                                <div className='flex w-full flex-row h-16 bg-[#c2cad0] p-4 justify-between items-center '>
+                                    <div className='w-[22%] text-left  text-3xl text-blue-700 font-bold'>
+                                        Newegg
+                                    </div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>65000</div>
+                                </div>
+                                <div className='flex w-full flex-row h-16 bg-[#c2cad0] p-4 justify-between items-center '>
+                                    <div className='w-[22%] text-left  text-3xl text-blue-700 font-bold'>amazon</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>65000</div>
+                                </div>
+                                <div className='flex w-full flex-row h-16 bg-[#c2cad0] p-4 justify-between items-center '>
+                                    <div className='w-[22%] text-left  text-3xl text-blue-700 font-bold'>BH</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>65000</div>
+                                </div>
+                                <div className='flex w-full flex-row h-16 bg-[#c2cad0] p-4 justify-between items-center '>
+                                    <div className='w-[22%] text-left  text-3xl text-blue-700 font-bold'>Best Buy</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>65000</div>
+                                </div>
+                                <div className='flex w-full flex-row h-16 bg-[#c2cad0] p-4 justify-between items-center '>
+                                    <div className='w-[22%] text-left text-3xl text-blue-700 font-bold'>memory</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>$542</div>
+                                    <div className='w-[22%] text-center '>65000</div>
+                                </div>
+                            </div>
+                            <div className='absolute top-0 w-full h-full flex flex-row justify-between px-8 '>
+                                <div className='w-[24%] '></div>
+                                <div className='w-[24%] bg-[#8dafbd] h-full opacity-30 text-white text-4xl flex items-center justify-between py-4 flex-col'>
+                                    <div className=''>Live Price</div>
+                                    <div >$523</div>
+                                </div>
+                                <div className='w-[24%] h-full bg-[#90bda6] opacity-30 text-white text-4xl flex items-center justify-between py-4 flex-col'>
+                                    <div >Last Price</div>
+                                    <div >$523</div>
+                                </div>
+                                <div className='w-[24%] h-full bg-[#caa3a2] opacity-30 text-white text-4xl flex items-center justify-between py-4 flex-col'>
+                                    <div >Total</div>
+                                    <div >4523</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
         </div>
     );
 }
