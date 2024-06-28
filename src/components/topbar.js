040660ce8d64d275c080580abc81e7e17693e5d8
sayjun0505 <sayjun0505@gmail.com>
2024-06-28 09:31:26 -0700

    a

--- src/components/topbar.js
+++ src/components/topbar.js
@@ -257,7 +257,7 @@ export default function Topbar() {
                 >
                   <li>
                     <a
-                      href="#"
+                      href="/"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Dashboard
@@ -265,7 +265,7 @@ export default function Topbar() {
                   </li>
                   <li>
                     <a
-                      href="#"
+                      href="/"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Settings
@@ -273,7 +273,7 @@ export default function Topbar() {
                   </li>
                   <li>
                     <a
-                      href="#"
+                      href="/"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Earnings
@@ -281,7 +281,7 @@ export default function Topbar() {
                   </li>
                   <li>
                     <a
-                      href="#"
+                      href="/"
                       className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                     >
                       Sign out
