b422ce2f6c28e3270432acc2d72028c5ee3dc626
sayjun0505 <sayjun0505@gmail.com>
2024-06-28 07:13:49 -0700

    a

--- src/layouts/MainLayout.js
+++ src/layouts/MainLayout.js
@@ -1,4 +1,4 @@
-import Header from '../components/header';
+// import Header from '../components/header';
 import Bottom from '../components/bottom';
 import Footer from '../components/footer';
 import Topbar from '../components/topbar';
--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -4,6 +4,5 @@ import { Outlet } from "react-router-dom";
 export default function Welcome() {
     return (
         <Outlet />
-        // <Link to="/cpu"><span className='text-red-500'>Pick a CPU</span></Link>
     )
 }
