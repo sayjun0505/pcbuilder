8494a5915bc4556d2fbebc5e1f172bd794c871ab
sayjun0505 <sayjun0505@gmail.com>
2024-07-17 02:20:48 -0700

    a

--- src/pages/home.js
+++ src/pages/home.js
@@ -88,12 +88,37 @@ export default function Home() {
   const goToNextPage = () => {
     setCurrentPage((prevPage) => prevPage + 1);
   };
+  const init=async()=>{
+    setFiltervalue("");
+    const response = await axios.get(
+      `http://103.35.189.49:5000/api/alldata?filter=`
+    );
+    // const response = await axios.get("http://localhost:5000/api/alldata");
+    console.log(response.data.data);
+    seCount(response.data.count);
+
+    const tmp = response.data.data.map((item, index) => {
+      const cpuid = item._id;
+      // let ar = response.data.vendor.filter((item) => item.cpuid === cpuid);
+      let t = {
+        id: item._id,
+        img: item.imgurl,
+        title: item.name.replace("Cpu ", "").replace(" Processor", ""),
+        detail: item.detail,
+        link: item.link,
+        price: item.price
+      };
+      return t;
+    });
+    setAll(tmp);
+
+  }
   const FilterProcess=async()=>{
     const response = await axios.get(
       `http://103.35.189.49:5000/api/alldata?filter=${filtervalue}`
     );
     // const response = await axios.get("http://localhost:5000/api/alldata");
-    console.log(response.data.data);
+    // console.log(response.data.data);
     seCount(response.data.count);
 
     const tmp = response.data.data.map((item, index) => {
@@ -137,12 +162,17 @@ export default function Home() {
 
         <div className="flex flex-row justify-between items-center w-full">
           <div className="flex flex-row items-center gap-8 w-full">
-            <div className="bg-white border-2 border-gray-200 w-[320px] sm:w-1/2 h-12 rounded-xl px-4 flex justify-center items-center">
+            <div className="bg-white border-2 border-gray-200 w-[320px] sm:w-1/2 h-12 rounded-xl px-4 flex justify-center items-center gap-4">
               <input
                 type="text"
                 className="w-full outline-none bg-white"
+                value={filtervalue}
                 onChange={(event) => setFiltervalue(event.target.value)}
               />
+              {filtervalue&&
+              <svg onClick={init} fill="#000000" className="h-6 w-6 hover:cursor-pointer" viewBox="0 0 492 492">
+              <path className="text-gray-800 fill-current" d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872 c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872 c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052 L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116 c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952 c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116 c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z"></path>
+            </svg>}
             </div>
             <div className="flex justify-center w-20 border-2 rounded-lg h-12 border-gray-200 items-center hover:border-gray-500 hover:cursor-pointer" onClick={FilterProcess}>
               <svg
